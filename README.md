55fa24680d555b593d23e14056073af5fd6f710b
Wonjae Kim <wonjaek36@gmail.com>
2020-07-02 01:56:45 +0900

    add python problem

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Neouly_Coding_Test
 
 Java document: https://mongodb.github.io/mongo-java-driver/4.1/apidocs/mongodb-driver-sync/com/mongodb/client/package-summary.html
+
+Python document: https://api.mongodb.com/python/current/tutorial.html#getting-a-database
---
+++ python/main.py
@@ -0,0 +1,28 @@
+import pymongo
+from pymongo import MongoClient
+
+
+def connect_mongodb(host, port):
+	#TODO Fill MongoClient to connect localhost mongodb
+	return MongoClient()
+
+def get_database(client, db_name):
+	#TODO return database of mongodb
+	return client[db_name]
+
+def get_collection(db, coll_name):
+	return db[coll_name]
+
+def get_average_bytes(coll):
+	avg = 0
+	#TODO using collection.find()
+	for doc in coll.find():
+		pass
+
+if __name__ == "__main__":
+	client = connect_mongodb("localhost", 27017)
+	db = get_database(client, "config")
+	coll = get_collection(db, "data")
+
+	avg = get_average_bytes(coll)
+	print (avg)
